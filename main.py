75b01707f9de62e5bbad531f6fa50a3e594ad3f7
Apple12 <apple12@192.168.1.2>
2023-11-10 09:51:07 -0700

    first Structure

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes.utils import select_all_tables
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/select_all_tables/{table_name}")
+def select_all_tables_route(table_name: str):
+    return select_all_tables(table_name)  
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
