f05a94b018227d505383258519e8457e96ebbffc
Apple12 <apple12@192.168.1.2>
2023-11-10 10:24:51 -0700

    first structure v2

--- databases.py
+++ databases.py
@@ -1,23 +1,19 @@
-import pyodbc
+import pymssql
+
 
 def connection():
-    # Local
-    '''
-        server = "USPP01-48840917"
-    database = "nom035"
-    username = "nom035"
-    password = "admin035"
-    '''
-
-
     # Server
     server = "smartloans.database.windows.net"
     database = "smartloan"
     username = "adminsmart"
     password = "Admin#8605"
 
+    connection_string = {
+        "server": server,
+        "database": database,
+        "user": username,
+        "password": password,
+        "autocommit": True,
+    }
 
-    ## f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-    connection_string = f"Driver={{ODBC Driver 18 for SQL Server}};Server={server},Database= {database};Uid = {username};Pwd = {password};Encrypt = yes;TrustServerCertificate = no;ConnectionTimeout = 30"
-
-    return pyodbc.connect(connection_string)
+    return pymssql.connect(**connection_string)
