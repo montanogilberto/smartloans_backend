ee1d0293d535ff8bb3a3e98f6b7f4723b345d06b
Gilberto Montaño <44681263+montanogilberto@users.noreply.github.com>
2023-11-28 17:11:47 -0700

    Update databases.py

--- databases.py
+++ databases.py
@@ -1,12 +1,13 @@
-import pymssql
-import os
-
 def connection():
-    # Server
     server = os.getenv("DB_SERVER")
     database = os.getenv("DB_NAME")
     username = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
+
+    print(f"server: {server}, database: {database}, username: {username}, password: {password}")
+
+    if None in (server, database, username, password):
+        raise ValueError("One or more required environment variables are missing.")
 
     connection_string = {
         "server": server,
@@ -15,3 +16,5 @@ def connection():
         "password": password,
         "autocommit": True,
     }
+
+    return pymssql.connect(**connection_string)
