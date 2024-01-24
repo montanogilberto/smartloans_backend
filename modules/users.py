f17f6ff1d37bc97ad969af1239c09b1e36fb2d55
Apple12 <apple12@192.168.1.18>
2024-01-23 18:51:43 -0700

    users v3

--- modules/users.py
+++ modules/users.py
@@ -20,3 +20,38 @@ def users_sp(json_file: dict):
         return JSONResponse(content=result, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def all_users_sp():
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC [dbo].[sp_users_all]")
+
+        # Fetch all the results as a list of tuples
+        rows = cursor.fetchall()
+
+        # Concatenate JSON strings from all rows into one string
+        json_result = "".join(row[0] for row in rows)
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+def one_users_sp(json_file: dict):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC sp_users_one @pjsonfile = %s", (json.dumps(json_file)))
+
+        # Fetch the result as a JSON string
+        json_result = cursor.fetchone()[0]
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
--- routes_/users.py
+++ routes_/users.py
@@ -1,12 +1,28 @@
 from fastapi import APIRouter
-from modules.users import users_sp
+from modules.users import users_sp, all_users_sp, one_users_sp
 
 
 router = APIRouter()
 
-# Read login docstring from the file
-with open("docs_description/users.txt", "r") as file:
-    docstring = file.read()
-@router.post("/users",  summary="users CRUD", description=docstring)
-def login(json: dict):
+# Read users  docstring from the file
+with open("./docs_description/users.txt", "r") as file:
+    users_docstring = file.read()
+@router.post("/users",  summary="users CRUD", description=users_docstring)
+def users(json: dict):
     return  users_sp(json)
+
+
+# Read all users docstring from the file
+with open("./docs_description/users_all.txt", "r") as file:
+    users_all_docstring = file.read()
+@router.get("/all_users",  summary="all users", description=users_all_docstring)
+def all_users():
+    return  all_users_sp()
+
+
+# Read one user docstring from the file
+with open("./docs_description/users_one.txt", "r") as file:
+    user_one_docstring = file.read()
+@router.post("/one_users",  summary="one user", description=user_one_docstring)
+def one_users(json: dict):
+    return  one_users_sp(json)
