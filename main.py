fdec059bea38aca4d8c880cf945f96fe67bd92c8
Gilberto Montano Quihuis (International Supplier) <v-gilbertom@microsoft.com>
2024-04-07 16:48:07 -0700

    update code fix libraries

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ origins = [
     "http://localhost",
     "https://localhost:8100",
     "http://localhost:8101",
-    "http://localhost:60733"
+    "http://localhost:8000"
 ]
 
 app.add_middleware(
