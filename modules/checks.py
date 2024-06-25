c00dda12c6cae2b7e6fe440f0cfab55699e510e2
Gilberto Montano Quihuis (International Supplier) <v-gilbertom@microsoft.com>
2024-06-25 12:04:53 -0700

    adding checks module

---
+++ modules/checks.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from databases import connection
+import json
+
+app = FastAPI()
+conn = connection()
+
+def checks_sp(json_file: dict):
+    print(json_file)
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC [dbo].[sp_checks] @pjsonfile = %s", (json.dumps(json_file)))
+
+        # Fetch the result as a JSON string
+        json_result = cursor.fetchall()
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def all_checks_sp():
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC [dbo].[sp_checks_all]")
+
+        # Fetch all the results as a list of tuples
+        rows = cursor.fetchall()
+
+        # Concatenate JSON strings from all rows into one string
+        json_result = "".join(row[0] for row in rows)
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+def one_checks_sp(json_file: dict):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC sp_checks_one @pjsonfile = %s", (json.dumps(json_file)))
+
+        # Fetch the result as a JSON string
+        json_result = cursor.fetchone()[0]
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
