da33f308a94225e74061576b68ed2239d5398592
montanogilberto <montanogilberto@gmail.com>
2025-04-15 18:39:22 -0700

    adding list orders

--- modules/orders.py
+++ modules/orders.py
@@ -70,3 +70,23 @@ def one_orders_sp(json_file: dict):
         return JSONResponse(content=result, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def list_orders_sp():
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC [dbo].[sp_orders_list]")
+
+        # Fetch all the results as a list of tuples
+        rows = cursor.fetchall()
+
+        # Concatenate JSON strings from all rows into one string
+        json_result = "".join(row[0] for row in rows)
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
--- routes_/orders.py
+++ routes_/orders.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from modules.orders import orders_sp, all_orders_sp, one_orders_sp
+from modules.orders import orders_sp, all_orders_sp, one_orders_sp, list_orders_sp
 
 
 router = APIRouter()
@@ -26,3 +26,10 @@ with open("./docs_description/orders_one.txt", "r") as file:
 @router.post("/one_orders",  summary="one order", description=order_one_docstring)
 def one_orders(json: dict):
     return  one_orders_sp(json)
+
+# Read all orders docstring from the file
+with open("./docs_description/orders_list.txt", "r") as file:
+    orders_list_docstring = file.read()
+@router.get("/list_orders",  summary="list orders", description=orders_list_docstring)
+def list_orders():
+    return  list_orders_sp()
