b9f1023f7a2be0452e606ee2cadd7f0c23315d08
montanogilberto <montanogilberto@gmail.com>
2025-04-24 11:33:00 -0700

    adding one product orders

--- modules/orders.py
+++ modules/orders.py
@@ -116,3 +116,19 @@ def orders_tracking_status_sp(json_file: dict):
         return JSONResponse(content=result, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def products_one_orders_sp(json_file: dict):
+        try:
+            cursor = conn.cursor()
+            cursor.execute("EXEC [dbo].[sp_orders_products_one] @pjsonfile = %s", (json.dumps(json_file)))
+
+            # Fetch the result as a JSON string
+            json_result = cursor.fetchone()[0]
+
+            # Parse the JSON string to a Python dictionary
+            result = json.loads(json_result)
+
+            return JSONResponse(content=result, status_code=200)
+        except Exception as e:
+            return JSONResponse(content={"error": str(e)}, status_code=500)
