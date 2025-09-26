6b293a3e74fdb1712fb3df9c76954c3309e63b00
JoseContreras16 <contreras.9999@gmail.com>
2025-09-25 23:28:39 -0700

    user email

--- modules/users.py
+++ modules/users.py
@@ -55,3 +55,19 @@ def one_users_sp(json_file: dict):
         return JSONResponse(content=result, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def one_users_email_sp(json_file: dict):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC sp_users_one_email @pjsonfile = %s", (json.dumps(json_file)))
+
+        # Fetch the result as a JSON string
+        json_result = cursor.fetchone()[0]
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
