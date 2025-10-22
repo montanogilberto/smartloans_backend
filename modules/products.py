0e7a0645bde93d4234ab62fc19280921f8df18c1
montanogilberto <montanogilberto@gmail.com>
2025-10-21 18:01:36 -0700

    adding products category by company

--- modules/products.py
+++ modules/products.py
@@ -109,3 +109,19 @@ def by_company_products_sp(json_file: dict):
         return JSONResponse(content=result, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def by_company_products_category_sp(json_file: dict):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC sp_products_categories_by_company @pjsonfile = %s", (json.dumps(json_file)))
+
+        # Fetch the result as a JSON string
+        json_result = cursor.fetchone()[0]
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
