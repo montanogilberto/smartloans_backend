a7e3ca165c3b35f09a11d6cca9f317820a6dbb3a
montanogilberto <montanogilberto@gmail.com>
2026-01-21 13:02:55 -0700

    mercadoLibre v7

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 # Routers
 from routes_ import (
