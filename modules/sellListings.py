dff31da3d36f4fa265b0b53d5e35613e4725134a
montanogilberto <montanogilberto@gmail.com>
2026-02-19 11:29:59 -0700

    sellListing query

--- modules/sellListings.py
+++ modules/sellListings.py
@@ -23,3 +23,38 @@ def sellListings_sp(json_file: dict):
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
+def all_sellListings_sp():
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC [dbo].[sp_sellListings_all]")
+
+        # Fetch all the results as a list of tuples
+        rows = cursor.fetchall()
+
+        # Concatenate JSON strings from all rows into one string
+        json_result = "".join(row[0] for row in rows)
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def query_sellListings_sp(json_file: dict):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("EXEC sp_sellListings_query @pjsonfile = %s", (json.dumps(json_file)))
+
+        # Fetch the result as a JSON string
+        json_result = cursor.fetchone()[0]
+
+        # Parse the JSON string to a Python dictionary
+        result = json.loads(json_result)
+
+        return JSONResponse(content=result, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
