874e523b107eb73456a39127663762090c91c0b8
montanogilberto <montanogilberto@gmail.com>
2026-03-04 13:59:52 -0700

    adding module companiesBranches

---
+++ routes_/companiesBranches.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from modules.companiesBranches import companiesBranches_sp, all_companiesBranches_sp, one_companiesBranches_sp
+
+router = APIRouter()
+
+# Read companiesBranches.txt docstring from the file
+with open("./docs_description/companiesBranches.txt", "r") as file:
+    companiesBranches_docstring = file.read()
+@router.post("/companiesBranches", summary="companiesBranches CRUD", description=companiesBranches_docstring)
+def companiesBranches(json: dict):
+    return companiesBranches_sp(json)
+
+
+# Read all companiesBranches.txt docstring from the file
+with open("./docs_description/companiesBranches_all.txt", "r") as file:
+    companiesBranches_all_docstring = file.read()
+@router.get("/all_companiesBranches", summary="all companiesBranches", description=companiesBranches_all_docstring)
+def all_companiesBranches():
+    return all_companiesBranches_sp()
+
+
+# Read one companiesBranches.txt docstring from the file
+with open("./docs_description/companiesBranches_one.txt", "r") as file:
+    companiesBranches_one_docstring = file.read()
+@router.post("/one_companiesBranches", summary="one companiesBranches", description=companiesBranches_one_docstring)
+def one_companiesBranches(json: dict):
+    return one_companiesBranches_sp(json)
