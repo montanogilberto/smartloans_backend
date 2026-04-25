3d08d69ec0bb38bddb77e593dabf18fba08d9e14
montanogilberto <montanogilberto@gmail.com>
2026-04-24 17:42:47 -0700

    adding POSGMO URL v2 test

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://localhost:8100",
+    "http://localhost:5173",
     "capacitor://localhost",
     "ionic://localhost",
 
